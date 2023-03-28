95fff20ae26b16b049c0ff3c3762325b833c6ed8
AMR_KHALID77 <84184457+amrkhalid-star902@users.noreply.github.com>
2023-03-28 07:29:50 +0200

    Update hazardunit.v

--- hazardunit.v
+++ hazardunit.v
@@ -12,7 +12,7 @@ module hazardunit(
     output IFIDen // register between instruction fetch state and instruction decode state enable signal
     );
     
-    // The true data dependence when register is read and at the same time instruction is loading the same redister
+    // The true data dependence when register is read and at the same time instruction is loading the same register
     // in this satiuation a stall must be occured and the following instruction mustnot be fetched to prevent any
     // unexpected output pc also should be unenabled 
     assign Controlmux=(Memread ==1'b0)?1:(Rs1==Rd | Rs2==Rd)?0:1;
